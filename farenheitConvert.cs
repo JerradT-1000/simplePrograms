2456fc6137061a28ed576a5733558819d90558ea
Jerrad <Jerrad.stutzman@gmail.com>
2017-06-24 22:12:48 -0700

    Add files via upload
    this program converts Fahrenheit to Celsius and kelvins

---
+++ farenheitConvert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double farenheit;
+            double celcius;
+            double kelvins;
+
+            Console.WriteLine("Enter the temperature in Farenheit to convert: ");
+            farenheit = double.Parse(Console.ReadLine());
+
+            celcius = ((farenheit - 32) * 5 / 9);
+            kelvins = (((farenheit - 32) * 5 / 9) + 273.15);
+
+            Console.WriteLine("{0}F = {1:0.##}C", farenheit, celcius);
+            Console.WriteLine("{0}F = {1:0.##}k", farenheit, kelvins);
+            Console.ReadLine();
+
+        }
+    }
+}
