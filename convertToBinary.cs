f35b92b118d17dd68d51de31279237524bd265c3
Jerrad <Jerrad.stutzman@gmail.com>
2017-06-24 22:10:25 -0700

    Add files via upload
    this program converts a base 10 to binary

---
+++ convertToBinary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ft;
+            int inch;
+            double meter;
+            double centimeter;
+
+            Console.WriteLine("Enter your height in feet, don't include inches: ");
+            ft = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}ft. How many inches?: ", ft);
+            inch = int.Parse(Console.ReadLine());
+
+            meter = ((ft / 12 + inch) / 40);
+            centimeter = ((ft / 12 + inch) * 2.54);
+
+            Console.WriteLine("You are {0} meters tall.", meter);
+            Console.WriteLine("And {0} centimeters tall.", centimeter);
+
+
+
+        }
+    }
+}
